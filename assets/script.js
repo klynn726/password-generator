40c89cc26f0e23429f54d246bbbdff081f58fa94
klynn726 <49536087+klynn726@users.noreply.github.com>
2021-01-18 14:51:00 -0700

    generate password update

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,8 @@
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+
 //get length of password
 //check for integer between 8-128
 // var length = 0
@@ -5,7 +10,15 @@ var up = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", 
 var down = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var num = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 var sym = ["!","@","#","$","%","^","&","*","(",")","{","}"]
-var passwordText = []
+var passText = []
+var choices = []
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
 
 var getLength = function() {
   length = parseInt(prompt("Enter a password length between 8 - 128 characters"))
@@ -26,23 +39,23 @@ var getCharTypes = function() {
 
   var upper = confirm("Do you want upper case letters?")
   if (upper === true){
-  passwordText.push([up])
+  choices.push([up])
 }
 
   var lower = confirm("Do you want lower case letters?")
 if (lower === true){
-  passwordText.push([down])
+  choices.push([down])
  }
 
   var num = confirm("Do you want numbers?")
  if (num === true){
-  passwordText.push([num])
+  choices.push([num])
 }
 
 
   var special = confirm("Do you want special characters?")
 if (special === true){
-  passwordText.push([sym])
+  choices.push([sym])
 }
 
 
@@ -53,26 +66,23 @@ if (special === true){
   }
 }
 
+
 // add randomization to password array
-
-function random() {
-for (i = passwordText.length -1; i > 0; i--){
-  j = Math.floor(Math.random() * i)
-  k = passwordText[i]
-  passwordText[i] = passwordText[j]
-  passwordText[j] = passwordText
-}
-}
-
-
 // loop through random password array
-function loop(){
-for (var i = 0; i < length; i++){
-  random()
-  passwordText[i];
-  
+
+function loop (){
+  for (var i = 0; i < length; i++){
+
+for (i = passText.length -1; i > 0; i--){
+  j = Math.floor(Math.random() * i)
+  k = choices[i]
+  choices[i] = choices[j]
+  choices[j] = passText
+}
+passText = num.toString
 }
 }
+// }
 //one char of each type (min) in password
 // function charTypeCheck(getCharTypes){
 //   if (true)
@@ -80,25 +90,16 @@ for (var i = 0; i < length; i++){
 //   i >= 1;
 // }
 
-//password generated
+//generate password 
 
 function generatePassword() {
   getLength()
   getCharTypes()
   loop()
   // charTypeCheck()
+  writePassword()
+  return password
   }
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
